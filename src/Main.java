80a45ab368829e7eb5b0162b1b78ff15b2f62fb7
Pedro <p3dvvr@gmail.com>
2022-09-07 21:48:09 -0500

    added window with button

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
-public class Main {
-    //hello comment
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.awt.*;
+
+public class Main extends Application{
+
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("ATM");
+
+        Button button = new Button();
+        button.setText("Insert card information");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
